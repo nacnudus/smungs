3e45116e796b13216e8453e1148d9c3e0e6de419
Duncan Garmonsway <nacnudus@gmail.com>
2018-06-29 12:23:34 +0100

    Link to ozmarriage source

--- R/ozmarriage.R
+++ R/ozmarriage.R
@@ -1,9 +1,13 @@
 #' Data from the 2017 Australian Marriage Survey
 #'
 #' Returns the file path to a spreadsheet of results of the 2017 Australian
-#' Marriage Survey.
+#' Marriage Survey by the Australian Bureau of Statistics, which asked, "Should
+#' the law be changed to allow same-sex couples to marry?"
 #'
 #' @source
+#'
+#' [Australian Bureau of
+#' Statistics](http://www.abs.gov.au/ausstats/abs@.nsf/mf/1800.0)
 #'
 #' @export
 ozmarriage <- system.file("extdata", "ozmarriage.xlsx", package = "smungs")
