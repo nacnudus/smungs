035763ba2e20adae5eac2b14673a7ef384fd6903
Duncan Garmonsway <nacnudus@gmail.com>
2018-07-10 09:34:13 +0100

    Add math data

---
+++ R/math.R
@@ -0,0 +1,8 @@
+#' Math class and school performance
+#'
+#' Returns the file path to a spreadsheet of data about the performance of
+#' pupils in a math class and a school.  The source was an education
+#' programme in the USA, which has been anonymised and simplified.
+#'
+#' @export
+math <- system.file("extdata", "math.xlsx", package = "smungs")
