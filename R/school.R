95fe1dba207a61487b341bcf93882c5e340bf992
Duncan Garmonsway <nacnudus@gmail.com>
2018-07-21 21:43:38 +0100

    Rename math.xlsx school.xlsx

---
+++ R/school.R
@@ -0,0 +1,8 @@
+#' School performance
+#'
+#' Returns the file path to a spreadsheet of data about the performance of
+#' pupils in a school.  The source was an education programme in the USA, which
+#' has been anonymised and truncated to be manageable examples.
+#'
+#' @export
+school <- system.file("extdata", "school.xlsx", package = "smungs")
